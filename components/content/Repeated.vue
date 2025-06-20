8840300263c9e4ba978f7065eff08fbe5a7de74e
Hendrik Heil <hendrik@heil.sh>
2025-06-20 15:55:18 +0200

    feat: add repeated props

---
+++ components/content/Repeated.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  elements: {
+    prefix: string;
+    content: string;
+  }[];
+}>();
+</script>
+
+<template>
+    <ul>
+        <li v-for="element in elements">
+            {{ element.prefix }}: {{ element.content }}
+        </li>
+    </ul>
+</template>
