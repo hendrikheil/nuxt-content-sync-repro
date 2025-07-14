2ee417800539918289b98f3dc26b182433d1d5c5
Hendrik Heil <hendrik@heil.sh>
2025-07-14 17:48:36 +0200

    chore: add media component

---
+++ components/content/global/Media.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  image: {
+    src: string;
+    alt?: string;
+  };
+  image_url: string;
+  img: string;
+}>();
+</script>
+
+<template>
+    <img :src="image.src" :alt="image.alt || ''" />
+    <img :src="image_url" />
+    <img :src="img" />
+</template>
